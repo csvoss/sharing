88bf1bdb9639283a96e8f1287b9724959bff88af
Chelsea Voss <chelsea@khanacademy.org>
2015-08-12 20:17:10 -0700

    Render index from template

--- django_sharing/views.py
+++ django_sharing/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 
 def main(request):
-    return HttpResponse("Hello, world!")
+    with open('django_sharing/templates/index.html') as f:
+        t = Template(f.read())
+        return HttpResponse(t.render(Context({})))
+
+    # return HttpResponse("Hello, world!")
