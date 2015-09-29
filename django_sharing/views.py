d2a3dc2b27a2fbccfb0e3247fd8a1adb5a639827
Chelsea Voss <csvoss@mit.edu>
2015-09-29 01:39:43 -0400

    Make split_rent JSONP compatible

--- django_sharing/views.py
+++ django_sharing/views.py
@@ -12,7 +12,31 @@ def main(request):
         t = Template(f.read())
         return HttpResponse(t.render(Context({})))
 
+# https://coderwall.com/p/k8vb_a/returning-json-jsonp-from-a-django-view-with-a-little-decorator-help
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = simplejson.dumps(objects)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = simplejson.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+    return decorator
 
+
+
+@json_response
 def split_rent(request):
     """Calculate the split rent using Critch's code."""
     preferences = request.GET.get("preferences")
@@ -32,12 +56,8 @@ def split_rent(request):
     solution, envies, envy_free = robust_rental_harmony.rental_harmony(
         total_rent, values)
 
-    return HttpResponse(json.dumps({
+    return {
         'solution': repr(solution),
         'envies': repr(envies),
         'envyFree': envy_free,
-    }))
-
-    return HttpResponse(output+"\nWhere the data was: \n%s" % preferences)
-
-
+    }
